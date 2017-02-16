c7f2362eaacde898ee4065be840383954cae973a
david <david.tateyama@become.co.jp>
2017-02-16 10:58:36 +0900

    Reformat.

--- app/controllers/Import.scala
+++ app/controllers/Import.scala
@@ -11,17 +11,9 @@ import models.Vocabulary
  * Created by david on 17/02/15.
  */
 class Import @Inject() (vocabulary: VocabularyService) extends Controller {
-  def importWord(
-    sourceLanguage: Lang,
-    word: String,
-    targetLanguage: Lang,
-    translation: String) = Action { request =>
-    val added = vocabulary.addVocabulary(
-      Vocabulary(sourceLanguage, targetLanguage, word, translation))
-    if (added)
-      Ok
-    else
-      Conflict
+  def importWord(sourceLanguage: Lang, word: String, targetLanguage: Lang, translation: String) = Action { request =>
+    val added = vocabulary.addVocabulary(Vocabulary(sourceLanguage, targetLanguage, word, translation))
+    if (added) Ok else Conflict
   }
 
 }
