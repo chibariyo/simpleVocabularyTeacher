0b1199f8d331f559f1699658f868043a8d1719e9
david <david.tateyama@become.co.jp>
2017-02-16 11:59:08 +0900

    Reformat.

--- app/models/Vocabulary.scala
+++ app/models/Vocabulary.scala
@@ -5,8 +5,4 @@ import play.api.i18n.Lang
 /**
  * Created by david on 17/02/15.
  */
-case class Vocabulary(
-  sourceLanguage: Lang,
-  targetLanguage: Lang,
-  word: String,
-  translation: String)
+case class Vocabulary(sourceLanguage: Lang, targetLanguage: Lang, word: String, translation: String)
