872cde1c0c8ad036a528958abef3c44b40b1a3f8
david <david.tateyama@become.co.jp>
2017-02-16 12:04:57 +0900

    Add QuizActor for interactive vocabulary quiz. Add quizEndpoint handler method to accept WebSocket connection with an Actor. Add route to establish WebSocket connection.

--- app/controllers/Quiz.scala
+++ app/controllers/Quiz.scala
@@ -2,8 +2,10 @@ package controllers
 
 import javax.inject.Inject
 
+import akka.actor.{ Actor, ActorRef, Props }
+import play.api.Play.current
 import play.api.i18n.Lang
-import play.api.mvc.{ Action, Controller }
+import play.api.mvc.{ Action, Controller, WebSocket }
 import services.VocabularyService
 
 /**
@@ -26,4 +28,36 @@ class Quiz @Inject() (vocabulary: VocabularyService) extends Controller {
       NotAcceptable
     }
   }
+
+  def quizEndpoint(sourceLang: Lang, targetLang: Lang) = WebSocket.acceptWithActor[String, String] { request =>
+    out => QuizActor.props(out, sourceLang, targetLang, vocabulary)
+  }
+}
+
+class QuizActor(out: ActorRef, sourceLang: Lang, targetLang: Lang, vocabulary: VocabularyService) extends Actor {
+
+  private var word = ""
+
+  override def preStart(): Unit = sendWord()
+
+  def receive = {
+    case translation: String if vocabulary.verify(sourceLang, word, targetLang, translation) =>
+      out ! "Correct!"
+      sendWord()
+    case _ => out ! "Incorrect, try again!"
+  }
+
+  def sendWord() = {
+    vocabulary.findRandomVocabulary(sourceLang, targetLang).map { v =>
+      out ! s"Please translate '${v.word}'"
+      word = v.word
+    } getOrElse {
+      out ! s"I don't know any word for ${sourceLang.code} and ${targetLang.code}"
+    }
+  }
+}
+
+object QuizActor {
+  def props(out: ActorRef, sourceLang: Lang, targetLang: Lang, vocabulary: VocabularyService): Props =
+    Props(classOf[QuizActor], out, sourceLang, targetLang, vocabulary)
 }
